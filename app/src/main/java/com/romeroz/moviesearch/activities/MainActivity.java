9b3ad4a87eb25afaa9e86716423a3d9feff8b0c4
roman <acrobatlegend@gmail.com>
2016-08-23 17:11:08 -0400

    Toolbar fragment title

--- app/src/main/java/com/romeroz/moviesearch/activities/MainActivity.java
+++ app/src/main/java/com/romeroz/moviesearch/activities/MainActivity.java
@@ -7,7 +7,6 @@ import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
-import android.view.Menu;
 import android.view.MenuItem;
 
 import com.romeroz.moviesearch.R;
@@ -62,6 +61,8 @@ public class MainActivity extends AppCompatActivity
         mViewPager.setAdapter(mViewPagerAdapter);
         // Disable swiping left and right
         mViewPager.setPagingEnabled(false);
+        // Set title
+        getSupportActionBar().setTitle("Search");
     }
 
     @Override
@@ -74,27 +75,6 @@ public class MainActivity extends AppCompatActivity
         }
     }
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.main, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
-
-        return super.onOptionsItemSelected(item);
-    }
 
     @SuppressWarnings("StatementWithEmptyBody")
     @Override
@@ -115,7 +95,8 @@ public class MainActivity extends AppCompatActivity
             //mViewPager.setCurrentItem(2, false);
 
         }
-
+        // Set title
+        getSupportActionBar().setTitle(item.getTitle());
         DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
         drawer.closeDrawer(GravityCompat.START);
         return true;
